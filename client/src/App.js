ad16a03ee2ffdc48dc1c8b5447673c73d08d4125
M LavaT <mwhlavaty@gmail.com>
2023-09-23 17:26:22 +0200

    working game

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Container, TextField } from "@mui/material";
 import Game from "./Game";
+import InitGame from "./InitGame";
+
 import socket from "./socket";
 import CustomDialog from "./components/CustomDialog";
 
@@ -10,10 +12,33 @@ export default function App() {
   // indicates if a username has been submitted
   const [usernameSubmitted, setUsernameSubmitted] = useState(false);
 
+  const [room, setRoom] = useState("");
+  const [orientation, setOrientation] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  // resets the states responsible for initializing a game
+  const cleanup = useCallback(() => {
+    setRoom("");
+    setOrientation("");
+    setPlayers("");
+  }, []);
+
+  useEffect(() => {
+    // const username = prompt("Username");
+    // setUsername(username);
+    // socket.emit("username", username);
+
+    socket.on("opponentJoined", (roomData) => {
+      console.log("roomData", roomData);
+      setPlayers(roomData.players);
+    });
+  }, []);
+
   return (
     <Container>
       <CustomDialog
         open={!usernameSubmitted} // leave open if username has not been selected
+        handleClose={() => setUsernameSubmitted(true)}
         title="Pick a username" // Title of dialog
         contentText="Please select a username" // content text of dialog
         handleContinue={() => {
@@ -37,7 +62,22 @@ export default function App() {
           variant="standard"
         />
       </CustomDialog>
-      <Game />
+      {room ? (
+        <Game
+          room={room}
+          orientation={orientation}
+          username={username}
+          players={players}
+          // the cleanup function will be used by Game to reset the state when a game is over
+          cleanup={cleanup}
+        />
+      ) : (
+        <InitGame
+          setRoom={setRoom}
+          setOrientation={setOrientation}
+          setPlayers={setPlayers}
+        />
+      )}
     </Container>
   );
 }
--- client/src/InitGame.js
+++ client/src/InitGame.js
@@ -20,7 +20,17 @@ export default function InitGame({ setRoom, setOrientation, setPlayers }) {
         title="Select Room to Join"
         contentText="Enter a valid room ID to join the room"
         handleContinue={() => {
-          //   join a room
+          // join a room
+          if (!roomInput) return; // if given room input is valid, do nothing.
+          socket.emit("joinRoom", { roomId: roomInput }, (r) => {
+            // r is the response from the server
+            if (r.error) return setRoomError(r.message); // if an error is returned in the response set roomError to the error message and exit
+            console.log("response:", r);
+            setRoom(r?.roomId); // set room to the room ID
+            setPlayers(r?.players); // set players array to the array of players in the room
+            setOrientation("black"); // set orientation as black
+            setRoomDialogOpen(false); // close dialog
+          });
         }}
       >
         <TextField
@@ -46,6 +56,11 @@ export default function InitGame({ setRoom, setOrientation, setPlayers }) {
         variant="contained"
         onClick={() => {
           //   create a room
+          socket.emit("createRoom", (r) => {
+            console.log(r);
+            setRoom(r);
+            setOrientation("white");
+          });
         }}
       >
         Start a game
