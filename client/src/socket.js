737194dea1baab700b41bc25fcc72313cb7028eb
M LavaT <mwhlavaty@gmail.com>
2023-09-26 18:29:29 +0200

    new build

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"; // import connection function
 
 // Replace 'localhost:8080' with your AWS server's public IP address or domain name
-const socket = io('https://chess-server-dev.us-west-2.elasticbeanstalk.com'); // initialize websocket connection
+const socket = io('https://mattschessserver.xyz'); // initialize websocket connection
 
 export default socket;
