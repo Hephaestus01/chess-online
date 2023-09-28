e5a41e38639a2fed0c56d9d9d0b4f44c93738182
M LavaT <mwhlavaty@gmail.com>
2023-09-28 11:14:19 +0200

    Move Procfile to root directory

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"; // import connection function
 
 // Replace 'localhost:8080' with your AWS server's public IP address or domain name
-const socket = io('https://mattschessserver.xyz'); // initialize websocket connection
+const socket = io('https://secure-gorge-17081-6eba02744539.herokuapp.com/'); // initialize websocket connection
 
 export default socket;
